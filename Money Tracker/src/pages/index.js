63d18994d71115eb80a1c9cdbebc379f66f122eb
RumenserAmanda <s21910115@student.unklab.ac.id>
2022-05-09 21:04:39 +0800

    Adding Individual Money Tracker part

---
+++ Money Tracker/src/pages/index.js
@@ -0,0 +1,17 @@
+import SplashScreen from './SplashScreen';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import Home from './Home';
+import TransactionHand from './TransactionHand';
+import TransactionBank from './TransactionBank';
+import Transaction from './Transaction';
+
+export {
+  SplashScreen,
+  SignIn,
+  SignUp,
+  Home,
+  TransactionHand,
+  TransactionBank,
+  Transaction,
+};
